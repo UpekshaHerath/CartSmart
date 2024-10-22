8b9476ebffa63db6c101d0afbacab024e3ab1760
UpekshaHerath <upekshadilshan000@gmail.com>
2024-10-22 15:49:49 +0530

    first commit

---
+++ store_API/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const connectDB = require("./db/connect");
+
+
+// async errors
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>products</a>");
+});
+
+// products route
+
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+start();
